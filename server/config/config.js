d92bf240bd9363e0ab0da9337c340ff2ed3679f4
Benjamin Rodriguez <brodriguez3@ufl.edu>
2021-03-18 10:00:25 -0400

    template update
    added the mern stack template

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-//This file holds any configuration variables we may need
-//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
-
-module.exports = {
-    db: {
-        uri: 'mongodb+srv://brUser:<uUAmn5aIj943zSGJ>@cluster0.myko0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-};
